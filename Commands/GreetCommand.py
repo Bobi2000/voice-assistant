445b6e98808c57caf475117d169119349a557386
Bobi2000 <bobko_2000@abv.bg>
2022-05-30 23:01:24 +0300

    Init commit

---
+++ Commands/GreetCommand.py
@@ -0,0 +1,20 @@
+import datetime
+from Commands.Command import Command
+from IO.output import speak
+
+
+class GreetCommand(Command):
+    def __init__(self): pass
+    def parseInput(): pass
+
+    def execute():
+        hour = datetime.datetime.now().hour
+        if hour >= 0 and hour < 12:
+            speak("Hello, Good Morning")
+            print("Hello, Good Morning")
+        elif hour >= 12 and hour < 18:
+            speak("Hello, Good Afternoon")
+            print("Hello, Good Afternoon")
+        else:
+            speak("Hello, Good Evening")
+            print("Hello, Good Evening")
---
+++ main.py
@@ -0,0 +1,7 @@
+from Commands.GreetCommand import GreetCommand
+from IO.output import speak
+
+# speak("TF")
+
+something = GreetCommand
+something.execute()
